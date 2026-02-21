928c6e58cdcc90dd946ef227a690167e0e5ef784
Ayan-Sh0ikh <shykhayan40@gmail.com>
2026-02-21 14:35:08 +0530

    Delete WeCanMakeIt directory

--- WeCanMakeIt/admin.js
+++
@@ -1,7 +0,0 @@
-// middleware/admin.js
-module.exports = (req, res, next) => {
-  if (req.user?.role !== 'admin') {
-    return res.status(403).json({ success:false, error:'Admin access required.' });
-  }
-  next();
-};
